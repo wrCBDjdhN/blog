56ad11ef9893a374ce12ed6482997d4db8313884
wrCBDjdh <wrcbdjdh@gmail.com>
2026-04-10 21:41:11 +0800

    fix: prisma client export

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,7 +1,18 @@
 import { PrismaClient } from '@prisma/client'
 
-const globalForPrisma = global as unknown as { prisma: PrismaClient }
+declare global {
+  var prisma: PrismaClient | undefined
+}
 
-export const prisma = globalForPrisma.prisma || new PrismaClient()
+let prisma: PrismaClient
 
-if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma
+if (process.env.NODE_ENV === 'production') {
+  prisma = new PrismaClient()
+} else {
+  if (!global.prisma) {
+    global.prisma = new PrismaClient()
+  }
+  prisma = global.prisma
+}
+
+export { prisma }
